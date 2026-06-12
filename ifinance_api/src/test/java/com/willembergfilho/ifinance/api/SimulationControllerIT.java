44f961dc66007da1ad586b0b6389940b8b7cacc1
dev-willem <willembergfilho@gmail.com>
2026-06-11 21:25:51 -0300

    fix: add @AutoConfigureMockMvc to load web layer in Spring Boot 4 integration test

--- ifinance_api/src/test/java/com/willembergfilho/ifinance/api/SimulationControllerIT.java
+++ ifinance_api/src/test/java/com/willembergfilho/ifinance/api/SimulationControllerIT.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 @Testcontainers(disabledWithoutDocker = true)
 class SimulationControllerIT {
